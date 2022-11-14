0143bc9f70cce7b9e36b538d683521e4dae276b1
Elifio <91218388+ElifioJr@users.noreply.github.com>
2022-11-14 20:12:07 -0300

    Initial commit

---
+++ Data/MVCDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Models.Domain;
+
+namespace ProjetoMVC.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
